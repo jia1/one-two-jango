7efdefadab3cb97835890e205dc289aa9c84c06a
JIA YEE <jiayeerawr@gmail.com>
2017-09-22 09:47:26 -0700

    Initial commit
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn one_two_jango.wsgi -b :$PORT --chdir one_two_jango --log-file -
---
+++ one_two_jango/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for one_two_jango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "one_two_jango.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
